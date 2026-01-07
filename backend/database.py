db06e045c68a5672dd0ca9afbea74a4c0654963b
Albert De Villa <devillalbert@gmail.com>
2026-01-07 09:59:53 +0800

    initial backend deployment

--- backend/database.py
+++ backend/database.py
@@ -2,15 +2,19 @@ import os
 import mysql.connector
 from mysql.connector import Error
 from contextlib import contextmanager
+from dotenv import load_dotenv
+
+
+load_dotenv() 
 
 def get_connection():
    try:
         connection = mysql.connector.connect(
-            host=os.environ.get("MYSQL_HOST", "localhost"),
-            user=os.environ.get("MYSQL_USER", "root"),
-            password=os.environ.get("MYSQL_PASSWORD", ""),
-            database=os.environ.get("MYSQL_DATABASE", "health_monitoring"),
-            port=int(os.environ.get("MYSQL_PORT", 3306))
+            host=os.environ.get("DB_HOST"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+            database=os.environ.get("MYSQL_DATABASE"),
+            port=os.environ.get("DB_PORT")
         )
         return connection
    except Error as e:
